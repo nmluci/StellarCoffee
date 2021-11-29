f03d628fa2619ad436649a9ee322381298afd148
nmluci <yryuu@outlook.com>
2021-11-29 13:53:26 +0800

    [#10] fix(inventory): 'return' -> 'raise'

--- app/inventory/services.py
+++ app/inventory/services.py
@@ -2,13 +2,13 @@ from app.inventory.models import Item, Inventory
 
 def registerNewItem(metadata: Item):
     if not metadata.id:
-        return Exception("invalid item id")
+        raise Exception("invalid item id")
     if not metadata.name:
-        return Exception("invalid menu's name")
+        raise Exception("invalid menu's name")
     if not metadata.stock:
         metadata.stock = 0
     if not metadata.price:
-        return Exception("menu's price can't be zero")
+        raise Exception("menu's price can't be zero")
 
     newMenu = Inventory(
         id=metadata.id,
